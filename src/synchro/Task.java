c527a34273e6c3e95f73e650ed37e568a646dc1c
Chamara Ratnayake <c.k.b.ratnayake@gmail.com>
2019-05-08 15:59:38 +1200

    Updated synchro methods

--- src/synchro/Task.java
+++ src/synchro/Task.java
@@ -2,9 +2,20 @@ package synchro;
 
 public class Task implements Runnable {
     private String _taskName;
+    private Database _db;
+
+    public Task(String name, Database db) {
+        _taskName = name;
+        _db = db;
+    }
 
     @Override
     public void run() {
-
+        try {
+            _db.incrementCount(_taskName);
+            System.out.println(_taskName + " Count: " + _db.getCount());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
