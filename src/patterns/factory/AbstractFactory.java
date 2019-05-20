267c34281be7addc86aa9d7e8af9ca593839c3a7
Chamara Ratnayake <c.k.b.ratnayake@gmail.com>
2019-05-21 10:01:00 +1200

    Abstract Factory Demo

---
+++ src/patterns/factory/AbstractFactory.java
@@ -0,0 +1,13 @@
+package patterns.factory;
+
+abstract class AbstractFactory {
+    public Expression prototype;
+
+    public Expression makePhase() {
+        return prototype.clone();
+    }
+
+    public abstract Expression makeCompromise();
+
+    public abstract Expression makeGrade();
+}
---
+++ src/patterns/factory/Expression.java
@@ -0,0 +1,25 @@
+package patterns.factory;
+
+class Expression implements Cloneable{
+    public String str;
+
+    public Expression(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public Expression clone() {
+        Expression clone = null;
+        try {
+            clone = (Expression)super.clone();
+        } catch (CloneNotSupportedException ex) {
+            ex.printStackTrace();
+        }
+        return clone;
+    }
+
+    @Override
+    public String toString() {
+        return str;
+    }
+}
